78fcd1e5d75d92da317e4329218096d34b42fecf
Dali <dali@digital8.com.au>
2022-05-01 19:49:04 +1000

    init places endpoint

---
+++ utils/places/index.ts
@@ -0,0 +1,23 @@
+import axios from "axios"
+import { GOOGLE } from "../../constants/google";
+
+type TgetMacdonalsParams = {
+    lat : number,
+    lng : number
+}
+
+export const getMacdonals = ({lat,lng} : TgetMacdonalsParams) => {
+var config = {
+  method: 'get',
+  url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat}%2C${lng}&radius=1500&type=restaurant&keyword=macdonals&key=${GOOGLE.apiKey}`,
+  headers: { }
+};
+
+return axios(config)
+.then(function (response) {
+    return response.data.results;
+})
+.catch(function (error) {
+  console.log(error);
+});
+}
