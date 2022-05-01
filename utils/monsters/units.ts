4a6f5372be03bdcc827f4dd4ef45e328f7189afd
Dali <dali@digital8.com.au>
2022-05-01 19:47:55 +1000

    Add convertThousands()

---
+++ utils/monsters/units.ts
@@ -0,0 +1,13 @@
+//Makes numbers over one thousand readable
+export const convertThousands = (number: number) => {
+    //Add thousand dot
+    let tempNum =  number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+    //remove last 2 char
+    tempNum = tempNum.substring(0, tempNum.length - 2);
+
+    //remove last char if 0
+    if(tempNum[tempNum.length - 1] === '0'){
+        tempNum = tempNum.substring(0, tempNum.length - 2);
+    }
+    return tempNum + "K";
+}
