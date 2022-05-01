2a74f650d01188bdf556ab9983ca38780079e818
Dali <dali@digital8.com.au>
2022-05-01 20:01:30 +1000

    added global monster type

---
+++ globlalTypes.ts
@@ -0,0 +1,10 @@
+export type Tmonster = {
+    id : string
+    name : string,
+    type : string,
+    rating : number
+    hp : number,
+    attack : number,
+    defense : number
+    speed : string
+}
