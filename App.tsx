c6d959ec190119e96656e6c8cebae9024e86bb89
Dali <dali@digital8.com.au>
2022-05-01 12:22:46 +1000

    setup status bar

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@
 
 import React from 'react';
 import {
+  Platform,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -18,6 +19,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import {
   Colors,
@@ -26,90 +28,41 @@ import {
   LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+import { ScreenProvider } from 'responsive-native';
+import { COLORS } from './src/lib/colors';
+import LoginScreen from './src/screens/Login/LoginScreen';
 
-const Section: React.FC<{
-  title: string;
-}> = ({children, title}) => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const CustomStatusBar : React.FC<{isDarkMode : boolean}> = ({isDarkMode}) => {
+  const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+  return(
+  <View style={[{height : APPBAR_HEIGHT, backgroundColor : COLORS.secondary }]}>
+    <SafeAreaView>
+      <StatusBar translucent backgroundColor={COLORS.secondary} barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+    </SafeAreaView>
+  </View>
+  )
 };
 
 const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: isDarkMode ? COLORS.main : COLORS.main,
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <SafeAreaProvider style={{ flex: 1 }}>
+            <CustomStatusBar isDarkMode={isDarkMode} />
+            <SafeAreaView style={backgroundStyle}>
+            <ScreenProvider baseFontSize={16} >
+        </ScreenProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
+
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
 });
 
 export default App;
